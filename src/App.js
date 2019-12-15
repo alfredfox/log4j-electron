47dd522c254c3b81fd8b71335912ab8a255d3313
Moe Lubovac <moe.lubovac@nextiva.com>
2019-12-15 12:39:22 -0700

    adding tab, table, modals

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
+import ProductsTable from './ProductsTable'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+export default function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="Products" />
+            <Tab label="Categories" />
+            <Tab label="Events" />
+            <Tab label="Attributes" />
+          </Tabs>
+        </AppBar>
+      </Grid>
+      <Grid item xs={12}>
+        <TabPanel value={value} index={0}>
+          <ProductsTable />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Categories
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Events
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Attributes
+        </TabPanel>
+      </Grid>
+    </Grid>
+  );
+}
+
---
+++ src/ProductsTable.js
@@ -0,0 +1,211 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const columns = [
+  {
+    id: 'name',
+    label: 'Name',
+    minWidth: 170
+  },
+  {
+    id: 'displayName',
+    label: 'Display Name',
+    minWidth: 100
+  },
+  {
+    id: 'description',
+    label: 'Description',
+    minWidth: 170,
+  },
+  {
+    id: 'actions',
+    label: '',
+    minWidth: 200
+  }
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  tableWrapper: {
+    maxHeight: 800,
+    overflow: 'auto',
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function ProductsTable() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [products, setProducts] = React.useState([])
+
+  const [dialog, setDialog] = React.useState({ delete: false, product: false});
+
+  useEffect(() => {
+    axios.get('products.json')
+      .then(response => setProducts(response.data.Records))
+      .catch(error => console.log(error))
+  },[]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleEditClick = (rowId) => {
+    setDialog({...dialog, product: true})
+    console.log(rowId)
+  }
+
+  const handleDeleteClick = (rowId) => {
+    setDialog({...dialog, delete: true})
+    console.log(rowId)
+  }
+
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.tableWrapper}>
+      <Toolbar>
+        <Typography variant="h6">
+          Table of Products
+        </Typography>
+      </Toolbar>
+        <Table stickyHeader aria-label="sticky table" height="80vh">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  {columns.map(column => {
+                    const value = row[column.id];
+                      return (column.id === 'actions') ? (
+                        <TableCell>
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            className={classes.button}
+                            onClick={(e) => handleEditClick(row.id)}
+                          >
+                            Edit
+                          </Button>
+                          <Button
+                            variant="outlined"
+                            color="secondary"
+                            className={classes.button}
+                            onClick={(e) => handleDeleteClick(row.id)}
+                          >
+                            Delete
+                          </Button>
+                        </TableCell>
+                      ) : (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number' ? column.format(value) : value}
+                        </TableCell>
+                      )
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={products.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+      <Dialog open={dialog.product} onClose={() => setDialog({...dialog, product: false})}>
+        <DialogTitle>Edit Product</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField label="Name" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Display Name" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Description" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              Whatever
+            </Grid>
+          </Grid>
+          <DialogActions>
+            <Button onClick={() => setDialog({...dialog, product: false})} variant="contained">
+              Cancel
+            </Button>
+            <Button onClick={() => setDialog({...dialog, product: false})} color="primary" variant="contained">
+              Save
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog
+        open={dialog.delete}
+        onClose={() => setDialog({...dialog, delete: false})}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle>Delete Product?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this product?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialog({...dialog, delete: false})} variant="contained">
+            Cancel
+          </Button>
+          <Button onClick={() => setDialog({...dialog, delete: false})} color="secondary" variant="contained" autoFocus>
+            Yes, delete.
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Paper>
+  );
+}
